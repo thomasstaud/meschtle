7df395beba83df239ed4db08ed4ff9cfd8236b7c
Thomas Staud <thomas.staud05@gmail.com>
2025-11-07 17:30:10 +0100

    startpage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let name = $state("");
+
+	function print() {
+		console.log($state.snapshot(name));
+	}
+</script>
+
+<h1>Willkommen bei Meschtle!</h1>
+<input type="text" placeholder="Name" bind:value={name}/>
+<button onclick={print}>check</button>
+
+<style>
+	input {
+      outline: #ffb931 medium auto;
+  }
+</style>
